65fa3696f455b6f9407535e618628b6ddb79613a
mrmalik01 <mrmalik0k@gmail.com>
2025-01-02 09:34:06 +0000

    docs: checking the git user

--- api/config/responses.py
+++ api/config/responses.py
@@ -7,14 +7,14 @@ class BaseResponse(BaseModel):
     data: dict
     error: dict
 
-
-DEFAULT_RESPONSE = {
-        "statusCode": 0,
-        "body": "",
-        "isBase64Encoded": True | False,
-        "headers": {
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*"
-        }
-    }
+#
+# DEFAULT_RESPONSE = {
+#         "statusCode": 0,
+#         "body": "",
+#         "isBase64Encoded": True | False,
+#         "headers": {
+#             "Access-Control-Allow-Headers": "*",
+#             "Access-Control-Allow-Origin": "*",
+#             "Access-Control-Allow-Methods": "*"
+#         }
+#     }
